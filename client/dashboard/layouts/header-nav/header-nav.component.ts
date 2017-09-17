77e37203ea2caaafc858ab5c750e8896b168b7eb
Ritesh Ranjan <ritranjan@deloitte.com>
2017-09-17 22:03:29 +0530

    header fix

--- client/dashboard/layouts/header-nav/header-nav.component.ts
+++ client/dashboard/layouts/header-nav/header-nav.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit, ViewEncapsulation, AfterViewInit } from '@angular/core';
-import { Helpers } from '../../helpers';
 
 declare let mLayout: any;
 @Component({
     selector: "app-header-nav",
-    templateUrl: "./header-nav.component.html",
+    templateUrl: "header-nav.pug",
     encapsulation: ViewEncapsulation.None,
 })
 export class HeaderNavComponent implements OnInit, AfterViewInit {
