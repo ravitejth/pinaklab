696bf8538b524b38f5cf78f6a17b72d4f6393c3e
Ravitej <raviteja@life9sys.com>
2018-03-02 02:25:12 +0530

    home page draft

---
+++ client/website/home/main/main.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main',
+  styleUrls: [ './main.scss'],
+  templateUrl: './main.pug',
+})
+export class MainComponent  {
+}
--- client/website/website.module.ts
+++ client/website/website.module.ts
@@ -7,11 +7,13 @@ import { HeaderComponent }      from './header/header.component'
 import { FooterComponent }      from './footer/footer.component';
 import { HomeHeaderComponent }  from './home/home-header/home-header.component';
 import { HomeComponent }        from './home/home.component';
+import { MainComponent }        from './home/main/main';
 import { AboutComponent }       from './about/about.component';
 import { ProfileComponent}      from './profile/profile.component';
 import { ServicesComponent }    from './services/services.component';
 import { PortfolioComponent }   from './portfolio/portfolio.component';
 import { ContactComponent }     from './contact/contact.component';
+
 
 
 // ROUTING
@@ -36,7 +38,8 @@ import { SharedModule } from '../shared/shared.module';
       ServicesComponent,
       PortfolioComponent,
       ContactComponent,
-      ProfileComponent
+      ProfileComponent,
+      MainComponent
     ]
 })
 
